9e50a4c8ce4e50a1f6a25b5f181d06abd92a7626
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2023-11-08 01:09:17 +0100

    Working parser for enums, and messages

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,21 @@
 use pest::Parser;
-use picopb::parser::{PicoPBParser, Rule};
+use picopb::parser::{PicoPBParser, Rule, parse};
 
 const PROTO_DEF: &str = "syntax = \"proto2\"; \
                          message A{int32 int_field = 1;}";
 
 fn main() {
-    //let p = PicoPBParser::parse(Rule::proto_definition, PROTO_DEF)
-    //    .expect("expected to parse the proto def");
-
-    // dbg!(PicoPBParser::parse(Rule::message_field, "required string name = 1")
-    //     .expect("expected to parse the proto def"));
-    // dbg!(PicoPBParser::parse(
-    //     Rule::message_definition,
-    //     "message A {required string name = 1; }"
-    // )
-    // .expect("expected to parse the proto def"));
-    // dbg!(PicoPBParser::parse(Rule::COMMENT, "// this is a comment").expect("comment did not parse"));
-
-    //
     let example_field = "required string name = 2;";
-    dbg!(PicoPBParser::parse(Rule::message_field, example_field).unwrap());
+    PicoPBParser::parse(Rule::message_field, example_field).unwrap();
 
     let message = "message A { required string name = 2; }";
-    dbg!(PicoPBParser::parse(Rule::message_definition, message).unwrap());
+    PicoPBParser::parse(Rule::message_definition, message).unwrap();
 
     let message_several = "message A {
     required string name = 2;
     optional string name = 2;
 }";
-    dbg!(PicoPBParser::parse(Rule::message_definition, message_several).unwrap());
+    PicoPBParser::parse(Rule::message_definition, message_several).unwrap();
 
     //
     let example_proto = "syntax = \"proto2\";
@@ -38,22 +25,46 @@ fn main() {
          repeated int32 integers = 2;
      }";
     println!("{}", example_proto);
-    dbg!(PicoPBParser::parse(Rule::proto_definition, example_proto,).unwrap());
+    PicoPBParser::parse(Rule::proto_definition, example_proto,).unwrap();
 
     let example_proto2 = "
+    syntax = \"proto2\";
+    import \"common.proto\";
      message Responses {
          required string name = 1;
          required bool ok = 2;
          optional Error error = 2;
      }
-enum Error {
-    ERROR_INVALID_PASSWORD = 1;
-    ERROR_INVALID_USER = 2;
-
-}";
+     enum Error {
+         ERROR_INVALID_PASSWORD = 1;
+         ERROR_INVALID_USER = 2;
+    }";
 
     println!("{}", example_proto2);
-    dbg!(PicoPBParser::parse(Rule::proto_definition, example_proto2,).unwrap());
+    PicoPBParser::parse(Rule::proto_definition, example_proto2,).unwrap();
+
+    let result = parse("
+        syntax = \"proto2\";
+        import \"common.proto\";
+        import \"shared.proto\";
+
+        message Query {
+            required bytes key = 1;
+            required bytes opaque = 2;
+        }
+
+        message Resonse {
+            required bytes value = 1;
+            required bytes opaque = 2;
+            optional Error error = 3;
+        }
+
+        enum Error {
+            ERROR_INVALID_KEY = 1;
+            ERROR_NOT_FOUND = 2;
+        }
+    ");
+    dbg!(result);
 }
 
 #[cfg(test)]
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,296 @@
+use std::{collections::{BTreeMap, HashMap}, hash::Hash};
+use std::num::ParseIntError;
+
+use pest::{Parser, error::Error as PestError, iterators::Pair as PestPair, state, Span};
 /// This file contains the proto definition parser for proto v2
 /// It is implemented using pest
 use pest_derive::Parser;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[grammar = "parser.pest"] // relative to src
 pub struct PicoPBParser;
+
+#[derive(Debug)]
+pub enum ParserError {
+    InvalidProtoDefinition,
+    ExpectedStatement,
+    InvalidVersionDeclaration,
+    InvalidProtoVersion,
+    DuplicateProtoVersion,
+    ImportMustBeNonEmpty,
+    PestRuleError(PestError<Rule>),
+    ParseIntError(ParseIntError),
+    ExpectedButGot(String, String),
+}
+
+impl From<PestError<Rule>> for ParserError {
+    fn from(error: PestError<Rule>) -> Self {
+        ParserError::PestRuleError(error)
+    }
+}
+
+
+impl From<ParseIntError> for ParserError {
+    fn from(error: ParseIntError) -> Self {
+        ParserError::ParseIntError(error)
+    }
+}
+
+#[derive(Debug)]
+pub struct EnumType {
+    pub identifier: String,
+    pub pairs: BTreeMap<String, i32>,
+}
+
+#[derive(Debug)]
+pub enum FieldQualifier {
+    Optional,
+    Required,
+    Repeated
+}
+
+impl From<&str> for FieldQualifier {
+    fn from(s: &str) -> Self {
+        match s {
+            "optional" => FieldQualifier::Optional,
+            "required" => FieldQualifier::Required,
+            "repeated" => FieldQualifier::Repeated,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum FieldType {
+    UnboundedString,
+    UnboundedBytes,
+    String(usize),
+    Bytes(usize),
+    Bool,
+    Int32,
+    Int64,
+    Uint32,
+    Uint64,
+    MessageType(String, usize),
+    UnboundedMessageType(String),
+}
+
+impl FieldType {
+    pub fn from_str(s: &str, max_size: Option<usize>) -> Self {
+        match (s, max_size) {
+            ("string", None) => Self::UnboundedString,
+            ("bytes", None) => Self::UnboundedBytes,
+            ("string", Some(limit)) => Self::String(limit),
+            ("bytes", Some(limit)) => Self::Bytes(limit),
+            ("bool", _) => Self::Bool,
+            ("int32", _) => Self::Int32,
+            ("int64", _) => Self::Int64,
+            ("uint32", _) => Self::Uint32,
+            ("uint64", _) => Self::Uint64,
+            (s, Some(limit)) =>  Self::MessageType(s.to_string(), limit),
+            (s, None) =>  Self::UnboundedMessageType(s.to_string()),
+        }
+    }
+}
+
+pub type Identifier = String;
+pub type Ordinal = i32;
+
+#[derive(Debug)]
+pub struct MessageField {
+    qualifier: FieldQualifier,
+    field_type: FieldType,
+    identifier: Identifier,
+    ordinal: Ordinal,
+}
+
+#[derive(Debug)]
+pub struct MessageType {
+    pub identifier: String,
+    pub fields: BTreeMap<Ordinal, MessageField>,
+}
+
+#[derive(Debug)]
+pub enum Version {
+    Proto2,
+    Unknown,
+}
+
+#[derive(Debug)]
+pub struct ProtoParser {
+    pub version: Version,
+    pub imports: Vec<String>,
+    pub enum_types: HashMap<String, EnumType>,
+    pub message_types: HashMap<String, MessageType>,
+}
+
+type ParseResult = Result<ProtoParser, ParserError>;
+type EmptyParseResult = Result<(), ParserError>;
+
+impl ProtoParser {
+
+    fn parse_message_definition(&mut self, message_statement: PestPair<'_, Rule>) -> EmptyParseResult {
+        dbg!(&message_statement);
+
+        let mut inner = message_statement.into_inner();
+
+        let identifier = inner.next().ok_or( ParserError::ExpectedButGot("identifier".to_string(), "None".to_string()))?;
+        let identifier = Self::identifier_from_span(identifier.as_span());
+
+        let mut message_type = MessageType { identifier: identifier.clone(), fields: BTreeMap::new() };
+
+        for value in inner {
+            match value.as_rule() {
+                Rule::message_field => {
+                    let mut message_inner = value.into_inner();
+
+                    let qualifier = message_inner.next().ok_or(ParserError::InvalidProtoDefinition)?.as_str();
+                    let field_type = message_inner.next().ok_or(ParserError::InvalidProtoDefinition)?.as_str();
+                    let identifier = message_inner.next().ok_or(ParserError::InvalidProtoDefinition)?;
+                    let field_number = message_inner.next().ok_or(ParserError::InvalidProtoDefinition)?;
+
+                    // TODO:
+                    // optional options that can contain the max_size
+                    let max_size = None;
+
+                    let field_identifier = Self::identifier_from_span(identifier.as_span());
+                    let field_ordinal = Self::ordinal_from_span(field_number.as_span())?;
+
+
+                    let value = MessageField {
+                        qualifier: FieldQualifier::from(qualifier),
+                        field_type: FieldType::from_str(field_type, max_size),
+                        identifier: field_identifier,
+                        ordinal: field_ordinal
+                    };
+                    message_type.fields.insert(field_ordinal, value);
+                }
+                _ => unreachable!()
+            }
+        }
+        self.message_types.insert(identifier, message_type);
+
+        Ok(())
+    }
+
+    fn identifier_from_span<'i>(span: Span<'i>) -> String {
+        let identifier_str = span.as_str();
+        identifier_str.to_string()
+    }
+
+    fn string_from_span<'i>(span: Span<'i>) -> String {
+        let identifier_str = span.as_str();
+        identifier_str[1..identifier_str.len()-1].to_string()
+    }
+
+    fn ordinal_from_span<'i>(span: Span<'i>) -> Result<i32, ParserError> {
+        let ordinal_str = span.as_str();
+        let parse_result = ordinal_str.parse::<i32>();
+        parse_result.map_err(ParserError::from)
+    }
+
+    fn parse_enum_definition(&mut self, enum_statement: PestPair<'_, Rule>) -> EmptyParseResult {
+        let mut inner = enum_statement.into_inner();
+
+        let identifier = inner.next().ok_or( ParserError::ExpectedButGot("identifier".to_string(), "None".to_string()))?;
+        let identifier = Self::identifier_from_span(identifier.as_span());
+
+        let mut enum_type = EnumType { identifier: identifier.clone(), pairs: BTreeMap::new() };
+
+        for value in inner {
+            match value.as_rule() {
+                Rule::enum_field => {
+                    let mut enum_inner = value.into_inner();
+                    let identifier = enum_inner.next().ok_or(ParserError::InvalidProtoDefinition)?;
+                    let number = enum_inner.next().ok_or(ParserError::InvalidProtoDefinition)?;
+
+                    let field_identifier = Self::identifier_from_span(identifier.as_span());
+                    let field_ordinal = Self::ordinal_from_span(number.as_span())?;
+
+                    enum_type.pairs.insert(field_identifier, field_ordinal);
+                }
+                _ => unreachable!()
+            }
+        }
+        self.enum_types.insert(identifier, enum_type);
+        Ok(())
+    }
+
+    fn parse_block_statement(&mut self, statement: PestPair<'_, Rule>) -> EmptyParseResult {
+        println!("Parsing block");
+        dbg!(&statement);
+        match statement.as_rule() {
+            Rule::message_definition => self.parse_message_definition(statement),
+            Rule::enum_definition => self.parse_enum_definition(statement),
+            _ => Err(ParserError::ExpectedButGot("message or enum definition".to_string(), statement.as_str().to_string()))
+        }
+    }
+
+    fn parse_import_statement(&mut self, statement: PestPair<'_, Rule>) -> EmptyParseResult {
+        println!("Parsing import statement");
+        if let Some(value) = statement.into_inner().next() {
+            let slice = value.as_span().as_str();
+            if slice.len() == 2 {
+                return Err(ParserError::ImportMustBeNonEmpty);
+            }
+            self.imports.push(slice[1..slice.len()-1].to_owned())
+        }
+        Ok(())
+    }
+
+    fn parse_version_decl(&mut self, statement: PestPair<'_, Rule>) -> EmptyParseResult {
+        if let Some(value) = statement.into_inner().nth(0) {
+            // TODO: 
+            if value.as_span().as_str() == "\"proto2\"" {
+                self.version = Version::Proto2;
+                return Ok(())
+            } else {
+                return Err(ParserError::InvalidProtoVersion)
+            }
+        }
+        Err(ParserError::InvalidVersionDeclaration)
+    }
+
+    fn parse_statement(&mut self, statement: PestPair<'_, Rule>) -> EmptyParseResult {
+        println!("parsing statement");
+        let statement = match statement.as_rule() {
+            Rule::statement => statement.into_inner().nth(0).ok_or_else(|| ParserError::ExpectedStatement)?,
+            _ => return Err(ParserError::ExpectedButGot("Statement".to_string(), statement.as_node_tag().unwrap_or("<unknown>").to_string())),
+        };
+
+        match statement.as_rule() {
+            Rule::block_statement => self.parse_block_statement(statement.into_inner().next().ok_or(ParserError::ExpectedStatement)?),
+            Rule::import_statement =>  self.parse_import_statement(statement),  
+            _ => Err(ParserError::ExpectedButGot("block statement".to_string(), statement.as_node_tag().unwrap_or("<unknown>").to_string()))
+        }?;
+        Ok(())
+    }
+}
+
+pub fn parse(input: &str) -> ParseResult {
+    let parse = PicoPBParser::parse(Rule::proto_definition, input)?;
+
+    let mut output = ProtoParser { 
+        version: Version::Unknown,
+        imports: Vec::new(),
+        enum_types: HashMap::new(),
+        message_types: HashMap::new()
+    };
+
+    for pair in parse.into_iter() {
+        match pair.as_rule() {
+            Rule::proto_definition => {
+                for p in pair.into_inner() {
+                    match p.as_rule() {
+                        Rule::statement => output.parse_statement(p)?,
+                        Rule::version_decl => output.parse_version_decl(p)?,
+                        Rule::EOI => break,
+                        _ => return Err(ParserError::ExpectedButGot("Statement or version decl".to_string(), p.as_str().to_string())),
+                    };
+                }
+            }
+            _ => unreachable!()
+        }
+    }
+    Ok(output)
+}
